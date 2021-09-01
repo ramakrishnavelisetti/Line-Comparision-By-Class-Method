9ccae6324aa18fdf9d137076f43b0870fa3f6784
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-02 00:06:10 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/LCCM/LineCompByClass.java
@@ -0,0 +1,7 @@
+package com.bdlz.LCCM;
+
+public class LineCompByClass {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Computation program by class method");
+    }
+}
